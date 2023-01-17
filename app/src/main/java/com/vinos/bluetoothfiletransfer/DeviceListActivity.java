0fd169ae03860b4329654a55b6af4c05531de7e6
vinayak <vinayakmestri25@gmail.com>
2023-01-18 01:02:22 +0530

    device list activity added

--- app/src/main/java/com/vinos/bluetoothfiletransfer/DeviceListActivity.java
+++ app/src/main/java/com/vinos/bluetoothfiletransfer/DeviceListActivity.java
@@ -6,14 +6,11 @@ import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.net.Uri;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -26,9 +23,7 @@ import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity implements CustomAdapter.DeviceSelectionListener {
-
-    public static int BLUETOOTH_DEVICE_SELECT_CODE = 4354345;
+public class DeviceListActivity extends AppCompatActivity implements CustomAdapter.DeviceSelectionListener {
     BluetoothAdapter adapter;
     List<BluetoothDevice> devices;
     int MY_PERMISSIONS_REQUEST_ACCESS_COARSE_LOCATION = 3433;
@@ -42,14 +37,10 @@ public class MainActivity extends AppCompatActivity implements CustomAdapter.Dev
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_device_list);
         adapter = BluetoothAdapter.getDefaultAdapter();
         buttonBluetooth = ((TextView) findViewById(R.id.turnOnTextView));
         deviceList = (RecyclerView) findViewById(R.id.deviceList);
-        deviceList = (RecyclerView) findViewById(R.id.deviceList);
-        selectFile = (TextView) findViewById(R.id.selectFile);
-        sendButton = (TextView) findViewById(R.id.sendButton);
-        receiveButton = (TextView) findViewById(R.id.receiveButton);
         deviceList.setLayoutManager(new LinearLayoutManager(this));
         checkPermissions();
 
@@ -60,32 +51,7 @@ public class MainActivity extends AppCompatActivity implements CustomAdapter.Dev
                 startBluetooth();
             }
         });
-        sendButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
 
-                Intent intent = new Intent(MainActivity.this, DeviceListActivity.class);
-                startActivityForResult(intent, BLUETOOTH_DEVICE_SELECT_CODE);
-
-                if (selectedBluetoothDevice != null && selectedFile != null) {
-                    bluetoothConnectionService.startClient(selectedBluetoothDevice, selectedFile);
-                }
-
-            }
-        });
-        receiveButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                receiveButton();
-            }
-        });
-
-        selectFile.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                selectFile();
-            }
-        });
 
         if (!adapter.isEnabled()) {
             buttonBluetooth.setVisibility(View.VISIBLE);
@@ -93,12 +59,6 @@ public class MainActivity extends AppCompatActivity implements CustomAdapter.Dev
             buttonBluetooth.setVisibility(View.GONE);
         }
         getBondedDevices();
-    }
-
-    private void selectFile() {
-        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
-        intent.setType("*/*");
-        startActivityForResult(Intent.createChooser(intent, "Select file"), 32343);
     }
 
 
@@ -115,49 +75,31 @@ public class MainActivity extends AppCompatActivity implements CustomAdapter.Dev
         }
         try {
             bluetoothConnectionService.startServer();
-        }catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
 
     }
 
     @SuppressLint("MissingPermission")
-    private void getBondedDevices(){
+    private void getBondedDevices() {
         devices = new ArrayList<>(adapter.getBondedDevices());
-        CustomAdapter bluetoothDeviceArrayAdapter = new CustomAdapter(devices,this);
+        CustomAdapter bluetoothDeviceArrayAdapter = new CustomAdapter(devices, this);
         deviceList.setAdapter(bluetoothDeviceArrayAdapter);
     }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == BLUETOOTH_DEVICE_SELECT_CODE && resultCode == RESULT_OK) {
-            if (data.hasExtra("device")) {
-                selectedBluetoothDevice = (BluetoothDevice) data.getExtras().get("device");
-            }
-
-            return;
-        }
-        Uri uri = data.getData();
-        String fileAbsolutePath = RealPathUtil.getRealPath(this, uri);
-        Log.v("ABC", "Path : " + fileAbsolutePath);
-
-        File file = new File(fileAbsolutePath);
-        Log.v("ABC", "Path : name: " + file.getName() + " " + " size: " + file.length());
-
-    }
 
     private void checkPermissions() {
-        String[] permissions = {android.Manifest.permission.BLUETOOTH_CONNECT,
-                android.Manifest.permission.ACCESS_COARSE_LOCATION,
-                android.Manifest.permission.ACCESS_COARSE_LOCATION,
-                android.Manifest.permission.READ_EXTERNAL_STORAGE,
+        String[] permissions = {Manifest.permission.BLUETOOTH_CONNECT,
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.READ_EXTERNAL_STORAGE,
                 Manifest.permission.WRITE_EXTERNAL_STORAGE};
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED) {
 
             if (!ActivityCompat.shouldShowRequestPermissionRationale(
                     this,
-                    android.Manifest.permission.BLUETOOTH_CONNECT
+                    Manifest.permission.BLUETOOTH_CONNECT
             )
             ) {
                 ActivityCompat.requestPermissions(
@@ -180,7 +122,7 @@ public class MainActivity extends AppCompatActivity implements CustomAdapter.Dev
             }
             adapter.enable();
 
-        }else{
+        } else {
             buttonBluetooth.setVisibility(View.GONE);
         }
     }
@@ -189,6 +131,11 @@ public class MainActivity extends AppCompatActivity implements CustomAdapter.Dev
     @Override
     public void onDeviceSelected(BluetoothDevice bluetoothDevice) {
         this.selectedBluetoothDevice = bluetoothDevice;
-        Toast.makeText(this,bluetoothDevice.getName(),Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, bluetoothDevice.getName(), Toast.LENGTH_SHORT).show();
+
+        Intent intent = new Intent();
+        intent.putExtra("device", bluetoothDevice);
+        setResult(RESULT_OK, intent);
+        finish();
     }
 }
