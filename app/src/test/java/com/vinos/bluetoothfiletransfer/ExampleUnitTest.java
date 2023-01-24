f5367115c31c43d2871579efe893175bdd14dae8
vinayak <vinayakmestri25@gmail.com>
2023-01-24 08:54:12 +0530

    File test

--- app/src/test/java/com/vinos/bluetoothfiletransfer/ExampleUnitTest.java
+++ app/src/test/java/com/vinos/bluetoothfiletransfer/ExampleUnitTest.java
@@ -1,8 +1,13 @@
 package com.vinos.bluetoothfiletransfer;
 
+import static org.junit.Assert.assertEquals;
+
+import com.vinos.bluetoothfiletransfer.util.FileHelper;
+
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -10,8 +15,18 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ExampleUnitTest {
+
+
     @Test
-    public void addition_isCorrect() {
-        assertEquals(4, 2 + 2);
+    public void test_wrongPath_File_Store() {
+        ByteArrayOutputStream boas = new ByteArrayOutputStream(45);
+        boolean testNull = FileHelper.storeFile(new File("/storage/emulated/0/file1.txt"), boas);
+        assertEquals(false, testNull);
+    }
+
+    @Test
+    public void test_null_File_Store() {
+        boolean testNull = FileHelper.storeFile(null, null);
+        assertEquals(false, testNull);
     }
 }
